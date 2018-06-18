8d3cbe4828527462e78e8a1550f72ef9ed595808
Alexandru Blinda <axb1080@cs.bham.ac.uk>
2018-06-18 22:09:06 +0100

    Initial commit

---
+++ MatrixMultiplication.java
@@ -0,0 +1,13 @@
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+        if(args.length != 3) {
+
+            System.err.println("Usage: pass matrix input file, vector input file");
+        }
+
+        
+    }
+
+}
