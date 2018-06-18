ca0c64534a5c4a82395154a148514517ef3e9823
Alexandru Blinda <axb1080@cs.bham.ac.uk>
2018-06-19 00:19:22 +0100

    Changed some little stuff

--- Multiplication.java
+++ Multiplication.java
@@ -1,9 +1,9 @@
-class methods{
+class Methods{
     int multiplysum(Vector v, Matrix m){
         int i;
-        accumulator a = new accumulator();
-        for(i = 0; i < m.rows(); i++){
-            Thread t = new Thread(new multiplyrow(v,m,i,a));
+        Accumulator a = new Accumulator();
+        for(i = 0; i < m.rowptr.size(); i++){
+            Thread t = new Thread(new Multiplyrow(v,m,i,a));
             t.run();
         }
         while(a.count < i){
@@ -13,7 +13,7 @@ class methods{
     }
 }
 
-class accumulator{
+class Accumulator{
     int total = 0;
     int count = 0;
     public synchronized void add(int i){
@@ -22,24 +22,22 @@ class accumulator{
     }
 }
 
-class multiplyrow extends Thread{
+class Multiplyrow implements Runnable {
     Vector v;
     Matrix m;
     int row;
     int total;
-    accumulator a;
+    Accumulator a;
 
-    public void run(){
-        int row_start = m.rowptr.get(row);
-        
-    }
-
-    public void multiplyrow(Vector v, Matrix m, int row, accumulator a){
+    public Multiplyrow(Vector v, Matrix m, int row, Accumulator a){
         this.v = v;
         this.m = m;
         this.row = row;
         this.a = a;
     }
 
+    public void run(){
+        int row_start = m.rowptr.get(row);
+    }
 }
 
