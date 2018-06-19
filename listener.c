e7ca64e0c5a3154b3d6f6c60519083046c260c75
Dan Jones <dan_j@live.co.uk>
2018-06-19 12:27:37 +0100

    Fixed

--- listener.c
+++ listener.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int main(int argc, char * argv) {
+int main(int argc, char ** argv) {
     const int NUM_WORKERS = 99;
     const int WORK_PER_WORKER = 1000000;
     const int ITERATIONS = 100000000;
@@ -16,18 +16,18 @@ int main(int argc, char * argv) {
     // Give the workers their initial work
     for(int i = 1; i <= NUM_WORKERS; i++) {
         MPI_Ssend(&N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        N += WORK_PER_WORKER
+        N += WORK_PER_WORKER;
     }
 
     for(int i = 0; i < ITERATIONS; i++) {
         // Listen for a finished calculation
         long double result;
         MPI_Status status;
-        MPI_recv(&result, 1, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv(&result, 1, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
         DONE += WORK_PER_WORKER;
 
-        printf("There are %d workers still going. %d have finished!", N-DONE, DONE);  
+        printf("There are %lld workers still going. %lld have finished!", N-DONE, DONE);  
 
         PI += result;
         
@@ -37,7 +37,7 @@ int main(int argc, char * argv) {
         
     }
 
-    printf("PI is: %d\n", 4*PI/(double)N);
+    printf("PI is: %100.Lf\n", 4*PI/(double)N);
  
     MPI_Finalize();
     
