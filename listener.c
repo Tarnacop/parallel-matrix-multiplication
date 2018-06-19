e5987c01781ab9d4ed1c28ac5d5da315124d5911
tauomicronmu <tauomicronmurs@gmail.com>
2018-06-19 11:34:48 +0100

    actually pass stuff back

--- listener.c
+++ listener.c
@@ -3,10 +3,14 @@
 #include <mpi.h>
 
 int main(int argc, char * argv) {
+    const int WORK_PER_WORKER = 10;
+
     int N = 0;
     long double PI = 0.0;
 
     MPI_Init(argc, argv);
+ 
+    // TODO: initialise workers
 
     while(1) {
         // Listen for a finished calculation
@@ -18,6 +22,12 @@ int main(int argc, char * argv) {
         }
         PI += result;
         printf("PI: %d\n", PI);
+        
+        // Tell the worker where to start from next
+        int send_value = N;
+        MPI_send(&send_value, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD); 
+        N += WORK_PER_WORKER;
+        
     }
  
     MPI_Finalize();
